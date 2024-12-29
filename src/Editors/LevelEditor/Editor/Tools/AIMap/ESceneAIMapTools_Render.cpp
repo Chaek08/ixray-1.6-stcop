6525da9a26867a7f953cbbf5ca3727c849299604
ForserX <sv3nk@yandex.ru>
2024-12-29 13:24:52 +0300

    Skip Ai node render error

--- src/Editors/LevelEditor/Editor/Tools/AIMap/ESceneAIMapTools_Render.cpp
+++ src/Editors/LevelEditor/Editor/Tools/AIMap/ESceneAIMapTools_Render.cpp
@@ -76,7 +76,12 @@ void ESceneAIMapTool::OnRender(int priority, bool strictB2F)
                         if (nodes){
                             const Fvector	DUP={0,1,0};
                             const float st 	= (m_Params.fPatchSize*0.9f)*0.5f;
-                            for (AINodeIt it=nodes->begin(); it!=nodes->end(); it++){
+                            for (AINodeIt it=nodes->begin(); it!=nodes->end(); it++)
+                            {
+                                // #TODO: Пофиксить это дерьмо
+                                if (*it == nullptr)
+                                    continue;
+
                                 SAINode& N 	= **it;
 
 								Fvector v;	v.set(N.Pos.x-st,N.Pos.y,N.Pos.z-st);
